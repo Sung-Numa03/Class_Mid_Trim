3476cdb51da783be972cb6933fff2749a7a34117
Sung-Numa03 <sungnumafroko@gmail.com>
2024-03-12 11:13:16 +0000

    A function that calculates and displays the positive divisors of a given integer.

---
+++ Main.cpp
@@ -0,0 +1,49 @@
+//This program calculates and displays the positive divisors of a given integer.
+//By Anwar Abdul-Wajid Sung-Numa
+//Computer Science Level 300
+
+#include <iostream>
+
+
+void Display_Divisor(int numb)
+{
+	std::cout << "Positive divisors of " << numb << " in descending order:" << std::endl;
+
+    // Iterating from numb to 1 and check for divisors
+    for (int i = numb; i > 0; --i) {
+        if (numb % i == 0) {
+            std::cout << i << std::endl;
+        }
+    }
+}
+int main()
+{
+    std::cout << "This program calculates and displays the positive divisors of a given integer." << std::endl;
+    std::cout << "Enter a positive integer, or enter 0 or a negative integer to terminate the program." << std::endl;
+
+    while (true) {
+        int user_Input;
+        std::cout << "\nEnter a positive integer: ";
+        std::cin >> user_Input;
+
+        // Checks if the user wants to terminate the program
+        if (user_Input <= 0) {
+            std::cout << "Program terminated. Goodbye!" << std::endl;
+            break;
+        }
+
+        Display_Divisor(user_Input);
+
+        // Ask the user if they want to enter another integer
+        char choice;
+        std::cout << "Do you want to enter another integer? (y/n): ";
+        std::cin >> choice;
+
+        if (choice != 'y' && choice != 'Y') {
+            std::cout << "Program terminated. Goodbye!" << std::endl;
+            break;
+        }
+    }
+	
+	std::cin.get();
+}
